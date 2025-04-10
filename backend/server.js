5cde5bba2cbdf1beeae60e5618e550dbacf316e7
david-tw-tan <43765407+david-tw-tan@users.noreply.github.com>
2025-04-10 11:14:45 +0800

    Followup commit #2

--- backend/server.js
+++ backend/server.js
@@ -290,12 +290,12 @@ app.post('/api/get-revised-essay', async (req, res) => {
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
 
 // BELOW IS FOR TESTING ON LOCAL IPHONE
 
-// app.listen(3000, '0.0.0.0', () => {
-//   console.log('Server running on http://0.0.0.0:3000');
-// });
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on http://0.0.0.0:3000');
+});
